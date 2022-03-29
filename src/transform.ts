8d82ce5dcb07279dfb580a57755e247021a359c2
Rom <git@brillout.com>
2022-03-29 15:40:03 +0200

    fix: use `config.root` as `cwd` for `fast-glob` (#5)

--- src/transform.ts
+++ src/transform.ts
@@ -41,7 +41,7 @@ export async function transform(
 
   const staticImports = (await Promise.all(
     matches.map(async({ absoluteGlobs, options, index, start, end }) => {
-      const files = (await fg(absoluteGlobs, { dot: true, absolute: true }))
+      const files = (await fg(absoluteGlobs, { dot: true, absolute: true, cwd: root }))
         .map((i) => {
           const path = relative(dir, i)
           if (path === filename)
